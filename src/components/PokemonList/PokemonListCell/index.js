dbf2b9b948a8044664046907b1e5a3576dc057cf
Tim <tnaughton@srv04k1sacc.charterlab.com>
2020-01-22 10:09:56 -0700

    add viewing single pokemon

--- src/components/PokemonList/PokemonListCell/index.js
+++ src/components/PokemonList/PokemonListCell/index.js
@@ -1,16 +1,21 @@
-import React from 'react';
-import {View, Text, ImageBackground} from 'react-native-web'
-import idx from 'idx'
-import {style} from './style'
+import React          from 'react';
+import {
+  View,
+  Text,
+  ImageBackground
+}                     from 'react-native-web'
+import idx            from 'idx'
+import {Link}         from 'react-router-dom'
+import {style}        from './style'
 
 // type Props = {
 //   pokemon: {
 //     name: string,
-//     height: { 
+//     height: {
 //       minimum: string,
 //       maximum: string
 //     },
-//     weight: { 
+//     weight: {
 //       minimum: string,
 //       maximum: string
 //     }
@@ -19,26 +24,28 @@ import {style} from './style'
 export const PokemonListCell = function PokemonListCell(props){
   const { pokemon } = props
     return <View style={style.cell}>
+    <Link style={{flex: 1}} to={`/pokemon/${pokemon.id}`}>
       <ImageBackground
-        source={pokemon.image} 
+        source={pokemon.image}
         style={style.pokemonImage}>
         <View style={style.pokemonBio}>
-          <Text style={style.pokemonTitle}> 
+          <Text style={style.pokemonTitle}>
             {pokemon.name}
           </Text>
-          <Text style={style.pokemonText}> 
+          <Text style={style.pokemonText}>
             Height: {pokemon.height.minimum} to {pokemon.height.maximum}
           </Text>
-          <Text style={style.pokemonText}> 
+          <Text style={style.pokemonText}>
             Weight: {pokemon.weight.minimum} to {pokemon.weight.maximum}
           </Text>
-          <Text style={style.pokemonText}> 
+          <Text style={style.pokemonText}>
             Classification: {pokemon.classification}
           </Text>
-          <Text style={style.pokemonText}> 
+          <Text style={style.pokemonText}>
             # {pokemon.number}
           </Text>
         </View>
       </ImageBackground>
+      </Link>
     </View>
 }
